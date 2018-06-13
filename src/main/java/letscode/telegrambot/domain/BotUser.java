892b303359b7cef4f4fc3a9939b5ffa3e0ad3d0f
Andrey <popov.a83@bk.ru>
2018-06-13 10:44:53 +0200

    Скелет бота на springe

--- src/main/java/letscode/telegrambot/domain/BotUser.java
+++ src/main/java/letscode/telegrambot/domain/BotUser.java
@@ -6,6 +6,9 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+/**
+ * Add constructors, getters, setters
+ */
 @Entity
 @Table(name = "telegram_user")
 @Data
@@ -16,4 +19,47 @@ public class BotUser {
     private Boolean isBot;
     private String lastName;
     private String userName;
+
+    public BotUser() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Boolean getBot() {
+        return isBot;
+    }
+
+    public void setIsBot(Boolean bot) {
+        isBot = bot;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
